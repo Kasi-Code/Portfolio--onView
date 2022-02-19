2d55ba6cd9164830d0eb9f08b64ac8bbbfdd965f
Kasi-Code <64224482+Kasi-Code@users.noreply.github.com>
2022-02-19 17:46:33 +0000

    better BEM naming BUT overflow not working!!

--- js/pageSwitch.js
+++ js/pageSwitch.js
@@ -1,27 +1,25 @@
 
-$(".skills-btn").on("click", ()=> {
-    $("#skills-page").css("display", "block")
-    $(".card-2__skills").css("display", "block")
-    $("#about-me-page").css("display", "none")
+$(".about-me-btn").on("click", ()=> {
+    $("#about-me-page").css("display", "block")
+    $("#skills-page").css("display", "none")
     $("#project-page").css("display", "none")
     $("#hobby-page").css("display", "none")
 })
-$(".about-me-btn").on("click", ()=> {
-    $("#skills-page").css("display", "none")
-    $("#about-me-page").css("display", "block")
+$(".skills-btn, .skills-btn-page").on("click", ()=> {
+    $("#about-me-page").css("display", "none")
+    $("#skills-page").css("display", "block")
     $("#project-page").css("display", "none")
     $("#hobby-page").css("display", "none")
 })
 $(".project-btn").on("click", ()=> {
-    $("#skills-page").css("display", "none")
     $("#about-me-page").css("display", "none")
+    $("#skills-page").css("display", "none")
     $("#project-page").css("display", "block")
     $("#hobby-page").css("display", "none")
 })
 $(".hobby-btn").on("click", ()=> {
-    $("#skills-page").css("display", "none")
-    $(".card-2__skills").css("display", "none")
     $("#about-me-page").css("display", "none")
+    $("#skills-page").css("display", "none")
     $("#project-page").css("display", "none")
     $("#hobby-page").css("display", "block")
 })
